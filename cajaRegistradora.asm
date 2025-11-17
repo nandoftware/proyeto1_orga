54c35c3719c562179ac465a22c2d225ee76fc4a1
Gaston <gastonalejandro.garciag@gmail.com>
2025-11-17 16:00:07 -0400

    lo logre

--- cajaRegistradora.asm
+++ cajaRegistradora.asm
@@ -3,7 +3,7 @@
 .data
 truquito: .word 0 
 codigo: .space 11 # necesita 9B para un codigo (el sistema implementado lee cadenas de texto), 1B para el salto de linea, y  1B mas para el operador
-bienvMSJ: .asciiz "¡Bienvenido a la caja registradora! ingresa los codigos de los productos\n"
+bienvMSJ: .asciiz "¡Bienvenido a un nuevo dia en la caja registradora! ingresa los codigos de los productos\n"
 
 # vamos a guardar una lista enlazada con los productos de la compra actual,
 # para eso vamos a guardar el codigo del pruducto, el precio total, y su nombre
@@ -31,7 +31,9 @@ IGUAL: .asciiz  " = "
 X: .asciiz "\tX"
 MENOS: .asciiz "\t-"
 TOTAL: .asciiz "Total compra:\t$"
-
+STOCK: .asciiz " stock "
+LINEAS: .asciiz "------------------------\n" 
+TOTAL2: .asciiz "Total cierre de caja:\t$"
 .text
 j Bienvenida
 
@@ -135,9 +137,9 @@ ILCierreDelDia:
 	
 # Agregar Cabeza (AC)
 ACCierreDelDia: 
-	move $t9, $ra
-	jal RLCierreDelDia
-	beq $s1, 1, lito2
+	
+	#tenemos el productoi en ($t1) 
+	
 	lw $s1, cabezaCierre
 	
 	li $v0,9 
@@ -150,14 +152,26 @@ ACCierreDelDia:
 
 	# todo lo debemos sacar de la lista enlazada de la compra actual
 	
-	lw $t8, ($a1) # guardamos el codigo del producto
-	sw $t8, 4($s1) # meto el codigo en la lista de compra actual
-	sw $a3, 8($s1) # en $a3 esta la cantidad de productos de un mismo en este momento
-	sw $t4, 12($s1) # en $t4 estara la parte entera total
-	sw $t5, 16($s1) # en $t5 estara la parte decimal total
+	# el codigo
+	lw $t8, 4($t1)
+	sw $t8, 4($s1) 
 	
-	la  $t4, ($a1)
-nombre2:	lb $t8 16($t4)
+	# la cantidada
+	lw $t8, 8($t1)
+	sw $t8, 8($s1)
+	
+	# la parte entera
+	lw $t8, 12($t1)
+	sw $t8, 12($s1) 
+	
+	# la parte decimal
+	lw $t8, 16($t1)
+	sw $t8, 16($s1)
+	
+	
+	la  $t4, ($t1)
+nombre2:	
+	lb $t8 20($t4)
 	sb $t8, 20($s1)
 	beq $t8, 0, lito2
 	add $t4, $t4, 1
@@ -165,50 +179,266 @@ nombre2:	lb $t8 16($t4)
 	b nombre2
 	
 lito2:	
+	jr $ra
+
+# Actualizo producto de Cierre
+AProductoCierre:
+# parte decimal primero por si hay un carry
+	lw $t8, 16($t1) # parte decimal del de la lista Actual
+	lw $t7, 16($t2) # parte decimal del de la lista de Cierre
+	add $t7, $t8, $t7
 	
+	li $t3,0 # aqui esta el carry
+deci2:
+	blt $t7, 0x64, endDeci2	
+	sub $t7,$t7,0x64
+	add $t3, $t3, 1
+	b deci2
+endDeci2:
+	sw $t7, 16($t2)
+	
+	# ahora la parte entera
+	lw $t8, 12($t1) # parte entera del de la lista Actual
+	lw $t7, 12($t2) # parte entera del de la lista de Cierre
+	add $t7, $t8, $t7
+	add $t7, $t3, $t7
+	
+	sw $t7, 12($t2)
+	jr $ra
+
+RLActual2Cierre:
+	move $t0, $ra
+	lw $t1, raizActual
+	lw $t1, ($t1)
+	
+recorriendo:
+	beqz $t1, reocorrido 
+	lw $t2, raizCierre
+	lw $t2, ($t2)
+	
+RecorriendoInterno:
+
+	beqz $t2, RecorridoInterno
+	# verificamos si eciste el codigo 4($t1) en algun 4($t2)
+	lw $t3 4($t1)
+	lw $t4 4($t2)
+	beq $t3, $t4, HayEnCierre # el prodducto actual esta en Cierre
+	lw $t2, ($t2)
+	b RecorriendoInterno 
+HayEnCierre:
+	jal AProductoCierre
+	lw $t1, ($t1)
+	b recorriendo
+RecorridoInterno:
+	jal ACCierreDelDia
+	lw $t1, ($t1)
+	b recorriendo# si llegamos hasta aca es que el producto en Actual no esta en Cierre
+reocorrido:
+	jr $t0
+
+MostrarTotal:
+	lw $t1, raizActual
+	lw $t1, ($t1)
+	
+	li $t5, 0 # decimal total
+	li $t7,0 # aqui esta el carry
+calcDeci:
+	beqz $t1, Int
+	
+	lw $t3 16($t1) # decimales del elemento en ($t1)
+	add $t5, $t5, $t3
+	
+deci3:
+	blt $t5, 0x64, endDeci3	
+	sub $t5,$t5,0x64
+	add $t7, $t7, 1
+	b deci3
+endDeci3: # al terminar en $t5 queda la parte decimal que es
+	lw $t1, ($t1)
+	b calcDeci
+	
+Int: 
+	lw $t1, raizActual
+	lw $t1, ($t1)
+	
+	li $t6, 0 # entero total
+calcInt:
+	beqz $t1, SumarCarry
+	
+	lw $t3 12($t1) # decimales del elemento en ($t1)
+	add $t6, $t6, $t3
+	lw $t1, ($t1)
+	b calcInt
+	
+SumarCarry: 
+	add $t6, $t6, $t7
+	
+Mostrar:
+
+	li $v0, 4
+	la $a0, TOTAL
+	syscall
+	
+	li $v0, 1
+	move $a0, $t6
+	syscall
+	
+	li $v0, 4
+	la $a0, PUNTO
+	syscall
+	
+	li $v0, 1
+	move $a0, $t5
+	syscall
+	
+	li $v0, 4
+	la $a0, N
+	syscall
+
+	jr $ra
+	
+	
+MostrarTotal2:
+	lw $t1, raizCierre
+	lw $t1, ($t1)
+	
+	li $t5, 0 # decimal total
+	li $t7,0 # aqui esta el carry
+calcDeciCierre:
+	beqz $t1, IntCierre
+	
+	lw $t3 16($t1) # decimales del elemento en ($t1)
+	add $t5, $t5, $t3
+	
+deci4:
+	blt $t5, 0x64, endDeci4	
+	sub $t5,$t5,0x64
+	add $t7, $t7, 1
+	b deci4
+endDeci4: # al terminar en $t5 queda la parte decimal que es
+	lw $t1, ($t1)
+	b calcDeciCierre
+	
+IntCierre: 
+	lw $t1, raizCierre
+	lw $t1, ($t1)
+	
+	li $t6, 0 # entero total
+calcIntCierre:
+	beqz $t1, SumarCarryCierre
+	
+	lw $t3 12($t1) # decimales del elemento en ($t1)
+	add $t6, $t6, $t3
+	lw $t1, ($t1)
+	b calcIntCierre
+	
+SumarCarryCierre: 
+	add $t6, $t6, $t7
+	
+MostrarCierre:
+
+	li $v0, 4
+	la $a0, TOTAL2
+	syscall
+	
+	li $v0, 1
+	move $a0, $t6
+	syscall
+	
+	li $v0, 4
+	la $a0, PUNTO
+	syscall
+	
+	li $v0, 1
+	move $a0, $t5
+	syscall
+	
+	li $v0, 4
+	la $a0, N
+	syscall
+
+	jr $ra
+	
+
+MostrarListaCierre:
+	move $t9, $ra
+	lw $t1, raizCierre
+	lw $t1, ($t1)
+	
+MostrarProductosVendidos:
+	beqz $t1, MostrarCierreCaja
+	
+	# muestra el nombre 
+	li $v0, 4
+	la $t2, 20($t1)
+	move $a0, $t2
+	syscall
+	
+	la $a0, N
+	syscall
+	
+	la $a0, X
+	syscall
+	
+	# mostramos la cantidad
+	li $v0, 1
+	lw $a0, 8($t1)
+	syscall
+	
+	li $t2, 0x10010000
+	lw $t4, ($t2)
+	lw $t3, 4($t1) # codigo del producto en la lista
+buscaStock:
+
+	beq  $t4, $t3, TengoStock
+	add $t2, $t2, 0x20
+	lw $t4, ($t2)
+	b buscaStock
+TengoStock:
+	
+	li $v0, 4
+	la $a0, STOCK
+	syscall
+	
+	# muestro lo que queda en el stock
+	li $v0, 1
+	lw $a0, 4($t2)
+	syscall
+	
+	li $v0, 4
+	la $a0, DOLAR
+	syscall
+	
+	li $v0, 1
+	lw $a0, 12($t1)
+	syscall
+	
+	li $v0, 4
+	la $a0, PUNTO
+	syscall
+
+	li $v0, 1
+	lw $a0, 16($t1)
+	syscall
+	
+	li $v0, 4
+	la $a0, N
+	syscall
+	
+	lw $t1, ($t1)
+	b MostrarProductosVendidos
+
+MostrarCierreCaja:
+	li $v0, 4
+	la $a0, LINEAS
+	syscall
+	
+	li $v0, 4
+	la $a0, N
+	syscall
+	
+	jal MostrarTotal2
 	jr $t9
-	
-# Recorrer Lista (RL)
-RLCierreDelDia:
-	
-	lw $t6, raizCierre 
-	lw $t7, ($t6) # y aqui la direcion a la que apunta el primer word de $t6 (next) 
-	 
-
-	
-empieza2:
-	beqz $t7,termino2
-	lw $s2, 4($t7) # guardamos en $s2 el codigo del producto
-	lw $s3, ($a1) # guardamos el codigo del produto que estamos facturando
-	
-	 # si el codigo de algun elemento que esta en la lista es igual al de $a1
-	 # quire decir que ya habiamos metido $a1 en la lista de la compra total, NO debemos volverlo a meter
-	beq $s2, $s3, termino3
-	
-	lw $t6, ($t6)
-	lw $t7, ($t6)
-	
-	
-	b empieza2
-termino2:
-	# en $t6 esta el ultimo elemento
-	jr $ra
-termino3: #este es para facturar
-	bge $a3, 2, terminar4
-	# en este momento tienes en $t7 la direcion al elemento que ya sabes que esta repetido el la lista del cierre del dia
-	lw $t8, 8($t7) # aqui debe estar la cantidad de ese producto que se a vendido, y como en este momento se esta vendiendo otro, se le debe agregar 
-	add $t8, $a3, $t8
-	sw, $t8, 8($t7)
-	li $s1, 1 # coloca true en $s1
-	jr $ra
-terminar4: # este es para incrementar
-	lw $t8, 8($t7) # aqui debe estar la cantidad de ese producto que se a vendido, y como en este momento se esta vendiendo otro, se le debe agregar 
-	sub $t8, $t8, 1
-	add $t8, $a3, $t8
-	sw, $t8, 8($t7)
-	
-	jr $ra
-
 # la seccion de bienvenida, aqui nda mas coloco un mensaje de bienvenida,
 # tal vez deba hacer esto por dia. 
 Bienvenida: 	
@@ -245,6 +475,7 @@ ingresarCodigo:
 	beq $s0, 42, IncrementarProducto
 	beq $s0, 43, TerminarCompraActual
 	beq $s0, 45, RestarProducto
+	beq $s0, 47, TerminarDia
 	
 	
 	
@@ -279,12 +510,6 @@ FacturarProducto:
 	lw $t5, 12($a1)
 
 	jal ACCompraActual
-	
-	lw $t4, 8($a1)
-	lw $t5, 12($a1)
-	
-	jal ACCierreDelDia
-
 	
 	# imprime el nombre del producto
 	li $v0, 4
@@ -358,8 +583,6 @@ deci:
 	add $t3, $t3, 1
 	b deci
 endDeci:
-	li $s5, 0
-	add  $s5, $s5, $t1 # aca guardamos la parte decimal adicional a la que ya estaba en cierre del dia
 	sw $t1, 16($t6)
 	
 	
@@ -367,9 +590,6 @@ endDeci:
 	lw $t1 12($t6)
 	mul $t1, $t2, $t1
 	add $t1, $t3, $t1
-	
-	li $s6, 0
-	add $s6, $s6, $t1 # aca esta la parte entera nueva a adicionar en cierre
 	sw $t1, 12($t6)
 	
 	
@@ -403,32 +623,6 @@ endDeci:
 	li $v0, 4
 	la $a0, N
 	syscall
-	
-	# ahora hay que reflejar la cantidad en la lista del cierre del dia
-	# debemos recorrer la lista del cierre hasta encontrar el producto con el codigo que esta en 4($a1)
-	# vamos a aprovecharnos de un diseño acidental pero muy conveniente en RLCierreDelDia
-	# el ya se encarga de conseguir que el codigo d eun producto se a igual a lo que esta en 4($a1)
-	# tambien debemos eliminar lo que habia en la lista 
-	li $a3, 0
-	add $a3, $a3, $t2
-	jal RLCierreDelDia
-	
-	# adicionamos la parte entera
-	lw $s4, 16($t7)
-	add $s4, $s4, $s5
-	li $t3, 0
-deci2:
-	blt $s4, 0x64, endDeci2	
-	sub $s4,$s4,0x64
-	add $t3, $t3, 1
-	b deci2
-endDeci2:
-	sw $s4, 16($t7)
-	
-	lw $s4, 12($t7) # estos son los enteros que teniamos
-	add $s4, $s6, $s4 # esto es la suma acabamos de aumentar y lo que teniamos,
-	add $s4, $t3, $s4 # y la suma del pusible carry
-	sw $s4, 12($t7)
 
 	j CicloCompraActual
 	
@@ -509,16 +703,39 @@ finEliminacion:
 	j CicloCompraActual
 
 TerminarCompraActual:
-	# debemos mostrar el total
+
+	# quiere recorrer la lista actual y preguntar si un elemento alli ya estaba en la lista de cierrre
+	# para eso mientras recorro la lista actual tambien recorro toda la lista de cirre para ver si cada elemento por el que paso
+	# esta o no en la lista de cierre
+	# si esta, debo operar sobre la cantidad y el precio,
+	# si no esta debo agregarlo con todo
+	
+	jal RLActual2Cierre
+	jal MostrarTotal
+
+	# reseteamos lista actual
 	li $v0, 0
 	sw $v0, raizActual
 	sw $v0, cabezaActual
 	
-	li $v0, 4
-	la $a0, TOTAL
-	syscall
 	
 	j CicloCompraActual
+	
+TerminarDia:
+
+	# debo mastrar el cierre de caja, para eso debo crear una funcion
+	# que recorrar toda la lista de cierre y vaya imprimiendo sus elementos
+	
+	jal MostrarListaCierre
+	
+	li $v0, 0
+	sw $v0, raizActual
+	sw $v0, cabezaActual
+	
+	li $v0, 0
+	sw $v0, raizCierre
+	sw $v0, cabezaCierre
+	j Bienvenida
 Main: 
 
 
